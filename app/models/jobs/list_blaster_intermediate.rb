14a3fe0df374e5f21315e9c4b4f48770deb7fcf1
Scott Feinberg <scott@allout.org>
2014-03-28 08:53:40 +0200

    Add retry logic to these jobs in attempt to combat problem with them failing

--- app/models/jobs/list_blaster_intermediate.rb
+++ app/models/jobs/list_blaster_intermediate.rb
@@ -1,5 +1,9 @@
 module Jobs
   class ListBlasterIntermediate
+  	extend Resque::Plugins::Retry
+
+    @retry_limit = 3
+    @retry_delay = 5
     @queue = :list_cutter_queue
 
     def self.perform(intermediate_result_id)
--- app/models/jobs/send_autofire_email.rb
+++ app/models/jobs/send_autofire_email.rb
@@ -1,5 +1,9 @@
 module Jobs
   class SendAutofireEmail
+  	extend Resque::Plugins::Retry
+
+    @retry_limit = 3
+    @retry_delay = 5
     @queue = :send_user_email
 
     def self.perform(action_page_id, member_id, additional_tokens)
