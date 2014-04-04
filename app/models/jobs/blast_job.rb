bdf2ba6936fe2dbc7048b429001526a8b7fefaaf
Scott Feinberg <scott@allout.org>
2014-04-04 12:21:19 +0200

    Quick fix, borked the fact that Resque turns its args to strings, not symbols.
    Signed-off-by: Scott Feinberg <scott@allout.org>

--- app/models/jobs/blast_job.rb
+++ app/models/jobs/blast_job.rb
@@ -1,22 +1,20 @@
 module Jobs
   class BlastJob
-    extend Resque::Plugins::Retry
-    @retry_limit = 2
-    @retry_delay = 1200
     @queue = :blaster_queue
 
-    def self.perform(email_id, list_id, options)
-      list = List.find(list_id)
-      email = Email.find(email_id)
+    def self.perform(args)
+      list = List.find(args['list_id'])
+      email = Email.find(args['email_id'])
+      options = args['options']
 
       user_ids = list.filter_by_rules_excluding_users_from_push(email, options)
       email.deliver_blast_in_batches(user_ids)
       email.update_attributes(:sent => true, :sent_at => Time.now.utc)
-      email.blast.update_attribute(:run_at, nil)
-      email.blast.save!
+      email.update_attribute(:run_at, nil)
+      email.save!
       list.saved_intermediate_result.update_results_from_sent_email!(email, user_ids.count)
     rescue Exception => e
-      email.blast.update_attribute(:run_at, nil)
+      email.update_attribute(:run_at, nil)
       PushLog.log_exception(email, "N/A", e)
       raise
     end
