b9f5ee232be44eab1433134305f3b67cebcc2a24
Scott Feinberg <feinberg.scott@gmail.com>
2014-04-18 16:11:40 +0200

    Fixing UAE Updating
    Signed-off-by: Scott Feinberg <feinberg.scott@gmail.com>

--- app/models/unique_activity_by_email.rb
+++ app/models/unique_activity_by_email.rb
@@ -22,12 +22,13 @@ class UniqueActivityByEmail < ActiveRecord::Base
     update_email_activities only_events_created_after, time_now
   end
 
-  def self.force_update_all!(start_date)
+  def self.force_update!(start_date, end_date)
     only_events_created_after = start_date.to_datetime.utc.strftime('%Y-%m-%d %H:%M:%S')
+    only_events_created_before = end_date.to_datetime.utc.strftime('%Y-%m-%d %H:%M:%S')
     time_now = Time.now.utc.strftime('%Y-%m-%d %H:%M:%S')
 
-    update_other_activities only_events_created_after, time_now
-    update_email_activities only_events_created_after, time_now
+    update_other_activities_between only_events_created_after, only_events_created_before, time_now
+    update_email_activities_between only_events_created_after, only_events_created_before, time_now
   end
 
   def self.reset!
@@ -92,5 +93,60 @@ class UniqueActivityByEmail < ActiveRecord::Base
   def self.last_updated_time
     result = select("MAX(updated_at) AS updated_at").first.try(:updated_at) || Time.at(0)
     result.to_formatted_s(:db)
+  end
+
+  def self.update_other_activities_between(last_updated_at, upper_limit, time_now)
+    sql = <<-SQL
+      INSERT INTO `#{self.table_name}` (email_id, activity, total_count, updated_at)
+      SELECT email_id, activity, COUNT(DISTINCT email_id, activity, user_id) as count, TIMESTAMP('#{upper_limit}', '%d-%m-%Y %h:%i:%s')
+        FROM `#{UserActivityEvent.table_name}`
+        WHERE email_id IS NOT NULL
+        AND activity NOT IN ('email_viewed', 'email_sent', 'email_clicked', 'email_spammed')
+        AND created_at > '#{last_updated_at}'
+        AND created_at < '#{upper_limit}'
+        GROUP BY email_id, activity
+        ON DUPLICATE KEY UPDATE total_count = total_count + VALUES(total_count), updated_at = TIMESTAMP('#{upper_limit}', '%d-%m-%Y %h:%i:%s');
+    SQL
+
+    self.connection.execute(sql)
+  end
+
+  def self.pushes_with_emails_in_range(start_date, end_date)
+    Push.find_by_sql <<-SQL
+      SELECT *
+      FROM #{Push.table_name}
+      WHERE id IN (
+        SELECT DISTINCT(p.id)
+        FROM `#{Push.table_name}` p
+        JOIN `#{Blast.table_name}` b ON b.push_id = p.id
+        JOIN `#{Email.table_name}` e ON e.blast_id = b.id
+        WHERE e.updated_at > TIMESTAMP('#{start_date}')
+        AND e.updated_at < TIMESTAMP('#{end_date}')
+      )
+    SQL
+  end
+
+  def self.update_email_activities_between(last_updated_at, upper_limit, time_now)
+    pushes_with_emails_in_range(last_updated_at, upper_limit).each do |push|
+      update_email_activities_by_push_between(push, last_updated_at, upper_limit, time_now)
+    end
+  end
+
+  def self.update_email_activities_by_push_between(push, last_updated_at, upper_limit, time_now)
+    self.connection.execute query_for_activity_stats_by_push_between(push.id, last_updated_at, upper_limit, time_now, UserActivityEvent::Activity::EMAIL_SENT)
+    self.connection.execute query_for_activity_stats_by_push_between(push.id, last_updated_at, upper_limit, time_now, UserActivityEvent::Activity::EMAIL_VIEWED)
+    self.connection.execute query_for_activity_stats_by_push_between(push.id, last_updated_at, upper_limit, time_now, UserActivityEvent::Activity::EMAIL_CLICKED)
+    self.connection.execute query_for_activity_stats_by_push_between(push.id, last_updated_at, upper_limit, time_now, UserActivityEvent::Activity::EMAIL_SPAMMED)
+  end
+
+  def self.query_for_activity_stats_by_push_between(push_id, last_updated_at, upper_limit, time_now, activity)
+    "INSERT INTO #{self.table_name} (email_id, activity, total_count, updated_at)
+     SELECT email_id, '#{activity.to_s}', count(distinct email_id, user_id) as count, TIMESTAMP('#{upper_limit}', '%d-%m-%Y %h:%i:%s')
+     FROM #{Push.activity_class_for(activity).table_name}
+     WHERE push_id = #{push_id}
+     AND created_at > '#{last_updated_at}'
+     AND created_at < '#{upper_limit}'
+     GROUP BY email_id
+     ON DUPLICATE KEY UPDATE total_count = total_count + VALUES(total_count), updated_at = TIMESTAMP('#{upper_limit}', '%d-%m-%Y %h:%i:%s');"
   end
 end
