68747a1525dadea22b037d432b294ed2704783b1
Scott Feinberg <feinberg.scott@gmail.com>
2014-04-25 15:29:02 +0200

    Force update the stats

--- app/models/jobs/update_unique_activity_by_email.rb
+++ app/models/jobs/update_unique_activity_by_email.rb
@@ -4,7 +4,9 @@ module Jobs
     @queue = :counters
 
     def self.perform
-      UniqueActivityByEmail.update!
+      current_date = 7.days.ago
+	    next_date = 1.day.from_now
+	    UniqueActivityByEmail.force_update!(current_date, next_date)
     end
   end
 end
